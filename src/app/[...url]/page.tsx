c792c412b5803538790253a44f9b62bca82d87a2
Amit Gupta <amit.gupta.work12@gmail.com>
2024-09-21 00:56:22 +0530

    added upstash victor and connected another website into localhost:3000 and connected db

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -10,16 +10,20 @@ function reconstructedUrl({ url }: { url: string[] }) {
   const decodedComponents = url.map((component) =>
     decodeURIComponent(component)
   );
+  return decodedComponents.join("/");
 }
 
 const Page = async ({ params }: PageProps) => {
-  const reconstructedUrl = reconstructedUrl({ url: params.url as string[] });
+  const reconstructedUrlValue = reconstructedUrl({
+    url: params.url as string[],
+  });
   console.log("Params", params);
-  // await ragChat.context.add({
-  //     type:"html",
-  //     source:
 
-  // })
+  await ragChat.context.add({
+    type: "html",
+    source: reconstructedUrlValue,
+    config: { chunkSize: 200, chunkOverlap: 50 },
+  });
 
   return <h1>Hello</h1>;
 };
